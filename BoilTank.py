b7ea6bee32abf5302675690e2cf492c3dd340f7e
flagos <flagospub@gmail.com>
2015-12-31 17:02:55 +0100

    add boil tank and corresponding tests

---
+++ BoilTank.py
@@ -0,0 +1,73 @@
+from threading import Thread
+import time
+
+class BoilTank(Thread):
+
+    def __init__(self, start_heat_queue, start_boil_queue, period=1, testing_queue_input=None):
+        self.period = period
+        self.start_heat_queue    = start_heat_queue
+        self.start_boil_queue    = start_boil_queue
+        self.testing_queue_input = testing_queue_input
+        self.is_ready = False
+        self.boil_steps = []
+        self.start_time = 0
+        self.stop_time = 0
+        self.period = period
+        self.set_consign(None)
+
+        Thread.__init__(self)
+        pass
+
+
+    def set_tank_ready(self):
+        self.is_ready = True
+        pass
+
+    def add_boil_step(self, temperature, duration):
+        self.boil_steps.append({'temperature':temperature, 'duration':duration})
+
+    def run(self):
+        self.set_consign(None)
+        while 1:
+            self.start_heat_queue.get() # wait for enough water to boil
+            self.set_consign(self.boil_steps[0]['temperature'])
+            self.start_heat_queue.task_done()
+
+            self.start_boil_queue.get() # start the boil steps
+            while (self.boil_steps):
+                boil_step = self.boil_steps.pop(0)
+
+                self.set_consign(boil_step['temperature'])
+
+                while self.read_temperature() + 2 < boil_step['temperature']:
+                    time.sleep(self.period)
+
+                self.start_time = time.time()
+                while time.time() < self.start_time + boil_step['duration']:
+                    time.sleep(self.period)
+
+                pass
+            self.stop_time = time.time()
+            self.start_boil_queue.task_done()
+
+            self.start_chiller()
+            pass
+        pass
+
+    def start_chiller(self):
+        pass
+
+
+    def read_temperature(self):
+        if self.testing_queue_input is not None:
+            t = self.testing_queue_input.get()
+            self.testing_queue_input.task_done()
+            return t
+        else:
+            return 55
+            pass
+        pass
+
+    def set_consign(self, arg):
+        self.consign = arg
+        pass
---
+++ test_BoilTank.py
@@ -0,0 +1,50 @@
+import unittest
+import BoilTank
+import Queue
+import time
+
+
+class HotBoilTest(unittest.TestCase):
+
+    def setUp(self, saturation=50):
+        self.start_heat_queue  = Queue.Queue()
+        self.start_boil_queue = Queue.Queue()
+        self.input_test_queue = Queue.Queue()
+        self.boiltank = BoilTank.BoilTank(self.start_heat_queue, self.start_boil_queue, 0.01, self.input_test_queue)
+        self.boiltank.start()
+
+        pass
+
+    def test_with_one_step(self):
+        bk = self.boiltank
+        self.assertTrue(bk.consign is None)
+        bk.add_boil_step(95, 0.2)
+        self.assertTrue(bk.consign is None) # not heating
+
+        self.start_heat_queue.put(None)  # start heating
+        self.start_heat_queue.join() # blocking -- heat should be started
+        self.assertEqual(bk.consign, 95) # heating ok
+
+        for i in [80, 85, 90, 92]:
+            self.input_test_queue.put(i)
+
+        self.start_boil_queue.put(None) # launching steps
+
+        self.input_test_queue.join() # blocking
+        self.assertEqual(bk.start_time, 0)
+
+        self.input_test_queue.put(94)
+        self.input_test_queue.join() # blocking
+        time.sleep(bk.period)      # time to update start_time variable
+        self.assertNotEqual(bk.start_time, 0)
+
+        self.start_boil_queue.join() # blocking
+        self.assertTrue(bk.stop_time - bk.start_time > 0.2)
+        self.assertTrue(bk.stop_time - bk.start_time < 0.2 + bk.period)
+        pass
+
+
+    def tearDown(self):
+        self.boiltank._Thread__stop()
+
+unittest.main()
