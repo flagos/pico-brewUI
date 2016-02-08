e8f08c228db91de27501f96404adf694de6847c5
flagos <flagospub@gmail.com>
2016-02-08 07:37:36 +0100

    remove useless singleton definition

--- Pico.py
+++ Pico.py
@@ -4,13 +4,6 @@ import Queue
 import Recipe
 
 
-class Singleton(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
 class Pico:
 
     def __init__(self):
