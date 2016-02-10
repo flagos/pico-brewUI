2d6d18c4f65215196ad336d129c8aab9ac800009
flagos <flagospub@gmail.com>
2016-02-10 08:18:28 +0100

    reorganize template into several files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 
+import json
 
 app = Flask(__name__)
 
@@ -8,6 +9,19 @@ app = Flask(__name__)
 def main():
     return render_template('main.html')
 
+
+@app.route("/temperature/<tank>.json")
+def temperature(tank):
+    data = []
+    data.append( ["16:00","16:05","16:10","16:15","16:20","16:25"])
+    if (tank == "hot"):
+        data.append([68,68,68,72,78,78])
+    elif (tank == "mash"):
+        data.append([68,68,68,72,78,79])
+    else:
+        data.append([68,68,68,72,78,80])
+
+    return json.dumps(data)
 
 app.debug = True
 if __name__ == "__main__":
