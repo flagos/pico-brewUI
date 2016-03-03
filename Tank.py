0d785059b39cdae162a24ab51aeb88a7634f4f05
flagos <flagospub@gmail.com>
2016-03-03 22:01:48 +0100

    tank was massing

---
+++ Tank.py
@@ -0,0 +1,37 @@
+import time
+from PID import PID
+
+
+class List_max():
+
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.array    = []
+
+    def append(self, obj):
+        if (len(self.array) >= self.max_size):
+            self.array.remove(0)
+
+        self.array.append(obj)
+
+
+class Tank(PID):
+
+    def __init__(self):
+        self.temperature_samples = []
+        self.last_fill = 0
+
+        self.temperatures = List_max(10)
+        self.powers       = List_max(10)
+
+        PID.__init__(self)
+
+
+    def update_pid(self, value):
+        now = time.time()
+        if(now - self.last_fill > timedelta(minutes=5)):
+            self.last_fill = now
+            self.temperatures.append(value)
+
+        #compute pid
+        self.update(value)
