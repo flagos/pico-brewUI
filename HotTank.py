6181fa72ee1981195eb6ef9294f6bd23e20d7a9b
flagos <flagospub@gmail.com>
2016-07-26 15:53:20 +0200

    pep8

--- HotTank.py
+++ HotTank.py
@@ -4,7 +4,7 @@ import time
 
 class HotTank(Thread, Tank):
 
-    def __init__(self, saturation = 50, period=1, testing_queue_input=None, testing_queue_output=None):
+    def __init__(self, saturation=50, period=1, testing_queue_input=None, testing_queue_output=None):
         self.tank_name = "Hot"
         self.current_volume = 0
         self.target_volume = 0
