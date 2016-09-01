d3cddec0fad0f85a8fde55e48fd7998f5ce314dd
flagos <flagospub@gmail.com>
2016-09-01 12:51:50 +0200

    stupid print

--- MessengerController.py
+++ MessengerController.py
@@ -124,7 +124,6 @@ class MessengerController(object):
         self.messenger.wait_for_ack(ackid=self.commands.index('acknowledge'))
 
     def set_resistors(self, mash, boil, hot):
-        print(mash)
         self.messenger.send_cmd(self.commands.index('Resistor'), hot, mash, boil, 100 - mash - boil - hot)
 
 
